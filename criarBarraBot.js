a856a2bd3382efee418b4f7e092e283daa311767
TG-TGTG <150091030+TG-TGTG@users.noreply.github.com>
2024-12-24 20:55:03 -0300

    Update criarBarraBot.js

--- criarBarraBot.js
+++ criarBarraBot.js
@@ -32,18 +32,13 @@ function criarBarra(var_nome, var_avatar) {
     <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z"/>
   </svg>`;
   
-    var phoneAction = document.createElement("div"); // Alterado para um elemento de link <a>
-    phoneAction.className = "actions phone-action";
-    phoneAction.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-  </svg>`;
-  
+    
     userBar.appendChild(backButton);
     userBar.appendChild(avatar);
     userBar.appendChild(name);
     // userBar.appendChild(moreActions);
-    userBar.appendChild(phoneAction);
-    userBar.appendChild(attachmentAction);
+    //userBar.appendChild(phoneAction);
+   // userBar.appendChild(attachmentAction);
   
     if (elementoPai) {
       elementoPai.prepend(userBar);
@@ -101,8 +96,6 @@ function criarBarra(var_nome, var_avatar) {
         link.id   = cssId;
         link.rel  = 'stylesheet';
         link.type = 'text/css';
-        /*link.href = 'https://cdn.jsdelivr.net/gh/igorlemoes/template_typebot_whatsapp@0.0.2/style.css';
-		*/
         link.media = 'all';
         head.appendChild(link);
     }
